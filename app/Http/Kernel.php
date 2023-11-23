9fa8048c3bb3f8e2013d8d146ac1898754e28dc0
iker <iker.zamorano18@gmail.com>
2023-11-23 13:27:18 +0100

    h

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'log.successful.login' => \App\Http\Middleware\LogLogin::class,
     ];
 }
